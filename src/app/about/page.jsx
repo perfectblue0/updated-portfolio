2c0a0e78b3c8c483d28f2b2589e225361d7742bf
Paola_G <perfectblue@live.com>
2024-04-25 11:29:49 -0700

    removed scroll svgs' from page.jsx in about folder, created a new component ScrollSvg with scroll svg code and exported it, imported into page.jsx in about folder and pasted it to page.jsx

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import ScrollSvg from "@/components/scrollSvg";
 import { motion, useInView } from "framer-motion";
 import { useRef } from "react";
 
@@ -62,24 +63,7 @@ export default function AboutPage() {
             </svg>
           </div>
           {/* scroll svg */}
-          <motion.svg
-            initial={{ opacity: 0.1, y: 0 }}
-            animate={{ opacity: 1, y: "10px" }}
-            transition={{ repeat: Infinity, duration: 4, ease: "easeInOut" }}
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            width={70}
-            height={70}
-          >
-            <path
-              d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z"
-              stroke="#000000"
-              strokeWidth="1"
-            ></path>
-            <path d="M12 6V14" stroke="#000000" strokeWidth="1"></path>
-            <path d="M15 11L12 14L9 11" stroke="#000000" strokeWidth="1"></path>
-          </motion.svg>
+          <ScrollSvg />
         </div>
         {/* container skills */}
         <div ref={skillsRef} className="">
@@ -109,24 +93,7 @@ export default function AboutPage() {
           </motion.div>
         </div>
         {/* scroll svg */}
-        <motion.svg
-          initial={{ opacity: 0.1, y: 0 }}
-          animate={{ opacity: 1, y: "10px" }}
-          transition={{ repeat: Infinity, duration: 4, ease: "easeInOut" }}
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          width={70}
-          height={70}
-        >
-          <path
-            d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z"
-            stroke="#000000"
-            strokeWidth="1"
-          ></path>
-          <path d="M12 6V14" stroke="#000000" strokeWidth="1"></path>
-          <path d="M15 11L12 14L9 11" stroke="#000000" strokeWidth="1"></path>
-        </motion.svg>
+        <ScrollSvg />
         {/* container experience */}
         <div ref={expRef} className="flex flex-col justify-center pb-48 gap-12">
           {/* title experience */}
