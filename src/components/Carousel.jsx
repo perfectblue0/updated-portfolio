31643798fa41b1993b16d9b75d3bafa82679075a
Paola_G <perfectblue@live.com>
2024-04-27 16:11:32 -0700

    added alt description to projects, added state that gives style effect of fading in on load for the project images, added animate-bounce style to ScrollSvg component

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -7,55 +7,64 @@ export default function Carousel() {
     {
       project: `First React Portfolio`,
       img: `/vanPort.jpg`,
+      altText: `css code`,
       description: `This deployed project was my first application made with React and Vanilla CSS.`,
       website: `https://perfectblue0.github.io/c-20-React-Portfolio/`,
     },
     {
       project: `E-commerce Backend`,
       img: `/ecomImg.jpg`,
+      altText: `street sign that says e commerce`,
       description: `This is an undeployed project where I used Sequelize to interact with a MySQL database to build the backend of an e-commerce application.`,
       website: `https://github.com/perfectblue0/c-13-E-commerce-ORM?tab=readme-ov-file`,
     },
     {
       project: `Social Network API`,
       img: `/handsNet.jpg`,
+      altText: `hands of different people on a table`,
       description: `I built this API so users could share their thoughts, reactions to friends' thoughts, and create a list of their friends. This project is not deployed and uses Express.js for routing, Day.js to format dates, has a MongoDB database, and Mongoose ODM.`,
       website: `https://github.com/perfectblue0/c-18-NoSQL-Social-Net-API`,
     },
   ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [loaded, setLoaded] = useState(false);
 
   const prevSlide = () => {
     const firstSlide = currentIndex === 0;
     const newInd = firstSlide ? picsArray.length - 1 : currentIndex - 1;
     setCurrentIndex(newInd);
+    setLoaded(false);
   };
 
   const followingSlide = () => {
     const isLast = currentIndex === picsArray.length - 1;
     const newInd = isLast ? 0 : currentIndex + 1;
     setCurrentIndex(newInd);
+    setLoaded(false);
   };
 
   const currentProject = picsArray[currentIndex];
 
   return (
-    <div className="flex gap-2 flex-col">
+    <div className="flex flex-col gap-2 ">
       <div className="w-2/3 m-auto">
         <div className="mt-10">
           <div
-            className="flex flex-col items-center gap-3 rounded-lg"
+            className="flex flex-col items-center gap-3"
             key={currentProject.project}
           >
-            <div className="relative w-80 h-56 transition ease-out duration-500 xs:w-65 xs:h-53 md:w-96 md:h-64 lg:w-[500px] lg:h-[350px] ">
+            <div className="w-80 h-56 relative transition ease-out duration-500 xs:w-65 xs:h-53 md:w-96 md:h-64 lg:w-[500px] lg:h-[350px]">
               <Image
                 src={currentProject.img}
                 fill
                 sizes="(min-width: 1024px) 500px, (min-width: 640px) 384px, (max-width: 639px) 260px"
-                className="rounded-lg"
-                alt={currentProject.project}
                 priority
+                onLoad={() => setLoaded(true)}
+                className={`rounded-lg transition-opacity duration-700 ease-out ${
+                  loaded ? "opacity-100" : "opacity-0"
+                }`}
+                alt={currentProject.altText}
               />
             </div>
             <div className="flex flex-col items-center gap-3">
